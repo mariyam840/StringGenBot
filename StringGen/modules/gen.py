b34bd04fe89da73c06c3b28b86263858a728741d
mariyam840 <159921630+mariyam840@users.noreply.github.com>
2024-03-08 18:11:28 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@FallenAssociation"))
+            await client(JoinChannelRequest("@ArmanChatz"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
